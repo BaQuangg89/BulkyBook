30b81a63d95d7effbbe1774e538cd6864104f71f
BaQuangg <baquang809@gmail.com>
2022-03-20 15:12:12 +0700

    Fix startup

--- BulkyBook/Startup.cs
+++ BulkyBook/Startup.cs
@@ -36,8 +36,7 @@ namespace BulkyBook
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
-            services.AddControllersWithViews().AddRazorRuntimeCompilation();
-            services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
